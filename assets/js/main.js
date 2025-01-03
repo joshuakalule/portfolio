ff35c79e6d53de2572b3956531845dddfb5840dc
Kalule Joshua <kaljoshkalule@gmail.com>
2025-01-03 17:04:02 +0300

    add html for backend-devops

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,33 +24,27 @@ if (toastTrigger) {
   })
 }
 
-// update modal content based on the title clicked
 modalIndentifier = {
   "Data Management Solutions": "./content/html/data-management.html",
   "Backend DevOps": "./content/html/backend-devops.html",
   "Electronics Design": "./content/html/electronics-design.html",
 };
-function updateModal(htmlPath) {
+
+// vary modal content based on clicked card
+var detailsModal = document.getElementById('detailsModal')
+detailsModal.addEventListener('show.bs.modal', function (event) {
+  var button = event.relatedTarget;
+  // fetch the parent of that button that has the class .service
+  var parentDiv = button.closest('.service');
+  var serviceTitle = parentDiv.querySelector('.service-title').textContent;
+  // Update the modal's content.
+  var modalTitle = detailsModal.querySelector('.modal-title');
+  modalTitle.textContent = serviceTitle;
+  htmlPath = modalIndentifier[serviceTitle];
   fetch(htmlPath)
     .then(response => response.text())
     .then(data => {
       document.querySelector('#detailsModal .modal-body .container-fluid').innerHTML = data;
     })
     .catch(error => console.error('Error fetching data-management.html:', error));
-}
-
-// vary modal content based on clicked card
-var detailsModal = document.getElementById('detailsModal')
-detailsModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget;
-  // fetch the parent of that button that has the class .service
-  var parentDiv = button.closest('.service');
-  // extract the title of that service
-  var serviceTitle = parentDiv.querySelector('.service-title').textContent;
-
-  // Update the modal's content.
-  var modalTitle = detailsModal.querySelector('.modal-title');
-  modalTitle.textContent = serviceTitle;
-  updateModal(modalIndentifier[serviceTitle]);
 })
